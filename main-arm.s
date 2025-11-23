b2720179728d01144fa235ae6c0f0133fed71d5a
kritomas <Unknown>
2025-11-23 12:21:28 +0100

    Y IT NO WORK

--- main-arm.s
+++ main-arm.s
@@ -5,7 +5,7 @@
 .section .vector_table, "a"
 .global _vector /*Expose _vector to linker script*/
 _vector: /*Vector map*/
-	.word __stack_top
+	.word __stack_top__
 	.word _start
 	.word 0
 	.word 0
@@ -18,7 +18,7 @@ _vector: /*Vector map*/
 .global _start
 .thumb_func
 _start:
-	ldr r0, =__stack_top
+	ldr r0, =__stack_top__
 	mov sp, r0 /*Set the stack pointer to the topr*/
 	bl main
 	b infinite_loop
--- main.c
+++ main.c
@@ -1,19 +1,41 @@
 // Constants from memory map
-const unsigned int SIO_BASE_ADDRESS = 0xD0000000;
-const unsigned int SIO_GPIO_OUT_OFFSET = 0x010; // GPIO 0-31
-const unsigned int SIO_GPIO_OE_OFFSET = 0x030; // GPIO 0-31
+const unsigned int PAD_CTL = 0x40038000;
+const unsigned int LED_PAD_OFFSET = 0x68;
+
+const unsigned ISO_BIT = 1 << 8;
+const unsigned IE_BIT = 1 << 6;
+const unsigned PUP_BIT = 1 << 3;
+
+const unsigned int IO_CTL = 0x40028000;
+const unsigned int LED_IO_OFFSET = 0x0cc;
+
+const unsigned char SIO_FUNC = 0x05;
+
+const unsigned int SIO_BASE_ADDRESS = 0xd0000000;
+const unsigned int SIO_GPIO_OUT_SET_OFFSET = 0x018; // GPIO 0-31
+const unsigned int SIO_GPIO_OE_SET_OFFSET = 0x038; // GPIO 0-31
 
 // The LED is on GPIO25
 const unsigned int LED_PIN = 1 << 25;
 
+const unsigned int GPIO_LOW_END = SIO_BASE_ADDRESS + SIO_GPIO_OUT_SET_OFFSET;
+const unsigned int GPIO_LOW_END_OUTPUT_ENABLE = SIO_BASE_ADDRESS + SIO_GPIO_OE_SET_OFFSET;
 
-const unsigned int GPIO_LOW_END = SIO_BASE_ADDRESS + SIO_GPIO_OUT_OFFSET;
-const unsigned int GPIO_LOW_END_OUTPUT_ENABLE = SIO_BASE_ADDRESS + SIO_GPIO_OE_OFFSET;
+const unsigned int LED_PAD_CTL = PAD_CTL + LED_PAD_OFFSET;
+const unsigned int LED_IO_CTL = IO_CTL + LED_IO_OFFSET;
 
 int main()
 {
+	volatile unsigned int* ledpadctl = (unsigned int*)LED_PAD_CTL;
+	volatile unsigned int* ledioctl = (unsigned int*)LED_IO_CTL;
 	volatile unsigned int* gpioctl = (unsigned int*)GPIO_LOW_END_OUTPUT_ENABLE;
 	volatile unsigned int* gpio = (unsigned int*)GPIO_LOW_END;
-	*gpioctl |= LED_PIN;
-	*gpio |= LED_PIN;
+	*(ledioctl + 0x2000/4) = SIO_FUNC;
+	*gpioctl = LED_PIN;
+	*(ledpadctl + 0x3000/4) = 1 << 8;
+	*gpio = LED_PIN;
+	while (1)
+	{
+		*gpio = LED_PIN;
+	}
 }
